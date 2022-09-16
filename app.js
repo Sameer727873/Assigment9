971f6b92983770941a31d7eeaa06a962ced2f4db
Sameer727873 <111059349+Sameer727873@users.noreply.github.com>
2022-09-16 06:25:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,155 @@
+// //Chapter 21 to 30
+// var a = prompt("Enter your city");
+// var b = a.toUpperCase
+// if(a === "karachi"){
+//     alert("Your city has been found")
+// }
+// else{
+//     alert("wrong")
+// }
+
+// var a = prompt("Enter your favorite mobile model")
+// var arr = ["iPhone 13" , "Samsung S10","Sony Xperia","infex hot 10"]
+// for(var i = 0 ; i < arr.length ; i++)
+// if(arr[i] === "iPhone 13"){
+//     alert("your Model has found")
+// }
+// else if(arr[i]==="Samsung S10"){
+//     alert("Your model has found")
+// 
+// else if(arr[i]==="Sony Xperia"){
+//     alert("Your model has found")
+// }
+// else if(arr[i]==="infex hot 10"){
+//     alert("Your model has found")
+// }
+// else{
+//     alert("not Found")
+// }
+// var a = "Pakistani"
+// var b = a.indexOf('n')
+// alert("The word found in " + b + " line")
+
+// var a = "islamabad"
+// var b = a.slice(0,5)
+// var c = b.replace("islam","Hyder")
+// var d = c + "abad"
+// alert(d)
+// var messege= "Ali and sami are friends and they play cricker and football together"
+// var a = messege.replace(/and/g,"&")
+// alert(a)
+
+// var a = 3.4514
+// var b = Math.round(a)
+// alert(b)
+// var b = Math.floor(a)
+// alert(b)
+// var b = Math.ceil(a)
+// alert(b)
+
+// var headuser = prompt("Head user name")
+// var tailuser = prompt("Tail user name")
+// var toss = Math.random()*2;
+// var floor = Math.floor(toss)
+// if(
+//     floor === 0
+// )
+// {
+//     alert("Head " + headuser + " win")
+// }
+// else{
+//     alert("Tail "+tailuser+ " win" )
+// }
+
+// var a = "50"
+// var b = Number(a)
+// // console.log(b)
+// var a = 472
+// a.toString
+// console.log(a)
+// var a = 66.6666
+// var b = a.toFixed(2)
+// alert(b)
+
+//Chapter 31 to 40
+
+// var a = new Date ()
+// console.log(a)
+
+// var a = new Date()
+// a.setMonth(11)
+// a.setDate(5)
+// a.setFullYear(2015)
+// a.setHours(22)
+// a.setMinutes(18)
+// a.setSeconds(11)
+// alert(a)
+
+// function abc(a,b){
+// return a + b
+// }
+// alert(abc(3,4))
+// alert(abc(3,7))
+// alert(abc(3,6))
+// alert(abc(3,8))
+
+
+
+// var abc = new Date()
+// abc.getTime();
+// console.log(abc)
+// if(a.getDay === 3){
+//     alert("its fun day")
+// }
+// else{
+//     alert("non")
+// }
+
+// var currentdate = new Date ("1-6-2002");
+// var todaydate = new Date ();
+// currentdate.setDate(21)
+// var currenttime = currentdate.getTime();
+// var todaytime  = todaydate.getTime();
+// var diff = todaytime - currenttime;
+// var finaldate = diff/(1000 * 60 * 60 * 24  * 365)
+// var floor = Math.floor(finaldate)
+// alert(floor)
+
+// var name = prompt("Enter  your Name");
+// var Month = prompt("Enter current month");
+// var unitno = prompt("Enter unit no");
+// var paymentdate = prompt("Enter your payment date")
+// var unitcharges  = 4.5;
+// var duedate = 4.8 * unitno ;
+// var calculate = unitno * unitcharges 
+// if(
+//     paymentdate > 15
+// ){
+//     alert(unitno)
+// }
+// else{
+//     alert(calculate)
+
+// function ABC (){
+//     var a = new Date()
+// a.setMonth(11)
+// a.setDate(5)
+// a.setFullYear(2015)
+// a.setHours(22)
+// a.setMinutes(18)
+// a.setSeconds(11)
+// alert(a)
+
+// }
+// ABC()
+// function abc (greet){
+//     alert(greet)
+// }
+// abc("Hello world")
+
+// function abc (a,b){
+// var a = a+b
+// alert(a)
+// return a
+// }
+// abc(2,4)
